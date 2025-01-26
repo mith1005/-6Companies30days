aef458593a0a885788da4be3a1a1e3d2562dca6e
mith1005 <mithileshrajput10@gmail.com>
2025-01-26 11:01:53 +0530

    Time: 10 ms (97.54%), Space: 93 MB (91.8%) - LeetHub

--- 2127-maximum-employees-to-be-invited-to-a-meeting/2127-maximum-employees-to-be-invited-to-a-meeting.cpp
+++ 2127-maximum-employees-to-be-invited-to-a-meeting/2127-maximum-employees-to-be-invited-to-a-meeting.cpp
@@ -2,55 +2,48 @@ class Solution {
 public:
     int maximumInvitations(vector<int>& favorite) {
         int n = favorite.size();
-        vector<vector<int>> reversedGraph(n);
+        vector<int> inDegree(n, 0);
+
         for (int person = 0; person < n; ++person) {
-            reversedGraph[favorite[person]].push_back(person);
+            inDegree[favorite[person]]++;
         }
 
-        auto bfs = [&](int startNode, unordered_set<int>& visitedNodes) -> int {
-            queue<pair<int, int>> q;
-            q.push({startNode, 0});
-            int maxDistance = 0;
-            while (!q.empty()) {
-                auto [currentNode, currentDistance] = q.front();
-                q.pop();
-                for (int neighbor : reversedGraph[currentNode]) {
-                    if (visitedNodes.count(neighbor)) continue;
-                    visitedNodes.insert(neighbor);
-                    q.push({neighbor, currentDistance + 1});
-                    maxDistance = max(maxDistance, currentDistance + 1);
-                }
+        queue<int> q;
+        for (int person = 0; person < n; ++person) {
+            if (inDegree[person] == 0) {
+                q.push(person);
             }
-            return maxDistance;
-        };
+        }
 
-        int longestCycle = 0, twoCycleInvitations = 0;
-        vector<bool> visited(n, false);
+        vector<int> depth(n, 1); 
+        while (!q.empty()) {
+            int currentNode = q.front();
+            q.pop();
+            int nextNode = favorite[currentNode];
+            depth[nextNode] = max(depth[nextNode], depth[currentNode] + 1);
+            if (--inDegree[nextNode] == 0) {
+                q.push(nextNode);
+            }
+        }
+
+        int longestCycle = 0;
+        int twoCycleInvitations = 0;
 
         for (int person = 0; person < n; ++person) {
-            if (!visited[person]) {
-                unordered_map<int, int> visitedPersons;
-                int current = person;
-                int distance = 0;
-                while (true) {
-                    if (visited[current]) break;
-                    visited[current] = true;
-                    visitedPersons[current] = distance++;
-                    int nextPerson = favorite[current];
-                    if (visitedPersons.count(nextPerson)) {  // Cycle detected
-                        int cycleLength = distance - visitedPersons[nextPerson];
-                        longestCycle = max(longestCycle, cycleLength);
-                        if (cycleLength == 2) {
-                            unordered_set<int> visitedNodes = {current,
-                                                               nextPerson};
-                            twoCycleInvitations +=
-                                2 + bfs(nextPerson, visitedNodes) +
-                                bfs(current, visitedNodes);
-                        }
-                        break;
-                    }
-                    current = nextPerson;
-                }
+            if (inDegree[person] == 0) continue; 
+
+            int cycleLength = 0;
+            int current = person;
+            while (inDegree[current] != 0) {
+                inDegree[current] = 0;  
+                cycleLength++;
+                current = favorite[current];
+            }
+
+            if (cycleLength == 2) {
+                twoCycleInvitations += depth[person] + depth[favorite[person]];
+            } else {
+                longestCycle = max(longestCycle, cycleLength);
             }
         }
 
