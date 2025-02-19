8aafff6c2dc0c7b4c8b8950d097030da185aad85
mith1005 <mithileshrajput10@gmail.com>
2025-02-19 11:29:09 +0530

    Time: 1 ms (79.3%), Space: 9.4 MB (74.09%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
     string getHappyString(int n, int k) {
-        int total = 3 * (1 << (n - 1));
+        stack<string> stringsStack;
+        int indexInSortedList = 0;
+        stringsStack.push(""); 
 
-       
-        if (k > total) return "";
+        while (stringsStack.size() > 0) {
+            string currentString = stringsStack.top();
+            stringsStack.pop();
 
-        string result(n, 'a');
+            if (currentString.size() == n) {
+                indexInSortedList++;
+                if (indexInSortedList == k) {
+                    return currentString;
+                }
+                continue;
+            }
 
-        unordered_map<char, char> nextSmallest = {
-            {'a', 'b'}, {'b', 'a'}, {'c', 'a'}};
-        unordered_map<char, char> nextGreatest = {
-            {'a', 'c'}, {'b', 'c'}, {'c', 'b'}};
-
-        
-        int startA = 1;
-        int startB = startA + (1 << (n - 1));
-        int startC = startB + (1 << (n - 1));
-
-        if (k < startB) {
-            result[0] = 'a';
-            k -= startA;
-        } else if (k < startC) {
-            result[0] = 'b';
-            k -= startB;
-        } else {
-            result[0] = 'c';
-            k -= startC;
-        }
-
-        for (int charIndex = 1; charIndex < n; charIndex++) {
-            
-            int midpoint = (1 << (n - charIndex - 1));
-
-            if (k < midpoint) {
-                result[charIndex] = nextSmallest[result[charIndex - 1]];
-            } else {
-                result[charIndex] = nextGreatest[result[charIndex - 1]];
-                k -= midpoint;
+            for (char currentChar = 'c'; currentChar >= 'a'; currentChar--) {
+                if (currentString.size() > 0 &&
+                    currentString.back() == currentChar)
+                    continue;
+                stringsStack.push(currentString + currentChar);
             }
         }
-
-        return result;
+        return "";
     }
 };
