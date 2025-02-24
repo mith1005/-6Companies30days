029c330d706c7939a6350f93656c5eb79260e379
mith1005 <mithileshrajput10@gmail.com>
2025-02-24 08:37:16 +0530

    Time: 187 ms (44.11%), Space: 163.1 MB (45.18%) - LeetHub

--- 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.cpp
+++ 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int mostProfitablePath(vector<vector<int>>& edges, int bob,
                            vector<int>& amount) {
-        int n = amount.size(), maxIncome = INT_MIN;
+        int n = amount.size();
         tree.resize(n);
         visited.assign(n, false);
-        queue<vector<int>> nodeQueue;
-        nodeQueue.push({0, 0, 0});
 
         for (vector<int> edge : edges) {
             tree[edge[0]].push_back(edge[1]);
@@ -17,31 +15,8 @@ public:
         findBobPath(bob, 0);
 
         visited.assign(n, false);
-        while (!nodeQueue.empty()) {
-            int sourceNode = nodeQueue.front()[0], time = nodeQueue.front()[1],
-                income = nodeQueue.front()[2];
+        findAlicePath(0, 0, 0, amount);
 
-            if (bobPath.find(sourceNode) == bobPath.end() ||
-                time < bobPath[sourceNode]) {
-                income += amount[sourceNode];
-            }
-
-            else if (time == bobPath[sourceNode]) {
-                income += (amount[sourceNode] / 2);
-            }
-
-            if (tree[sourceNode].size() == 1 && sourceNode != 0) {
-                maxIncome = max(maxIncome, income);
-            }
-            for (int adjacentNode : tree[sourceNode]) {
-                if (!visited[adjacentNode]) {
-                    nodeQueue.push({adjacentNode, time + 1, income});
-                }
-            }
-
-            visited[sourceNode] = true;
-            nodeQueue.pop();
-        }
         return maxIncome;
     }
 
@@ -49,6 +24,7 @@ private:
     unordered_map<int, int> bobPath;
     vector<bool> visited;
     vector<vector<int>> tree;
+    int maxIncome = INT_MIN;
 
     bool findBobPath(int sourceNode, int time) {
         bobPath[sourceNode] = time;
@@ -58,14 +34,37 @@ private:
             return true;
         }
 
-        for (auto adjacentNode : tree[sourceNode]) {
-            if (!visited[adjacentNode]) {
-                if (findBobPath(adjacentNode, time + 1)) {
-                    return true;
-                }
+        for (int adjacentNode : tree[sourceNode]) {
+            if (!visited[adjacentNode] && findBobPath(adjacentNode, time + 1)) {
+                return true;
             }
         }
+
         bobPath.erase(sourceNode);
         return false;
+    }
+
+    void findAlicePath(int sourceNode, int time, int income,
+                       vector<int>& amount) {
+        visited[sourceNode] = true;
+
+        if (bobPath.find(sourceNode) == bobPath.end() ||
+            time < bobPath[sourceNode]) {
+            income += amount[sourceNode];
+        }
+
+        else if (time == bobPath[sourceNode]) {
+            income += (amount[sourceNode] / 2);
+        }
+
+        if (tree[sourceNode].size() == 1 && sourceNode != 0) {
+            maxIncome = max(maxIncome, income);
+        }
+
+        for (int adjacentNode : tree[sourceNode]) {
+            if (!visited[adjacentNode]) {
+                findAlicePath(adjacentNode, time + 1, income, amount);
+            }
+        }
     }
 };
