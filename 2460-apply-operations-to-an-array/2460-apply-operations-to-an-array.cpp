d81e84933362846acc4a69abb3a0ea19dfe4db4d
mith1005 <mithileshrajput10@gmail.com>
2025-03-01 16:08:49 +0530

    Time: 0 ms (100%), Space: 13.1 MB (62.06%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
-        for (int index = 0; index < n - 1; index++) {
-            if (nums[index] == nums[index + 1] && nums[index] != 0) {
+        int writeIndex = 0;  
+
+        for (int index = 0; index < n; index++) {
+            if (index < n - 1 && nums[index] == nums[index + 1] &&
+                nums[index] != 0) {
                 nums[index] *= 2;
                 nums[index + 1] = 0;
             }
-        }
-        int nonZeroIndex = 0;
-        for (int iterateIndex = 0; iterateIndex < n; iterateIndex++) {
-            if (nums[iterateIndex] != 0) {
-                nums[nonZeroIndex++] = nums[iterateIndex];
+
+            if (nums[index] != 0) {
+                if (index != writeIndex) {
+                    swap(nums[index], nums[writeIndex]);
+                }
+                writeIndex++;
             }
         }
-        while (nonZeroIndex < n) {
-            nums[nonZeroIndex++] = 0;
-        }
+
         return nums;
     }
 };
